aba573cb5df9305ce3497dadbd675deb8a88a2cd
greymd <greengregson@gmail.com>
2017-11-20 18:32:40 +0900

    Fix scrit

--- build4unix.sh
+++ build4unix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 _this_dir=$(dirname $0)
-(echo "#!/usr/bin/env java -jar"; cat "${_this_dir}"/target/tlscurl-*.jar) > tlscurl
-chmod +x tlscurl
-tar -zcvf tlscurl_unix.tar.gz tlscurl
+mkdir "${_this_dir}/target"
+(echo "#!/usr/bin/env java -jar"; cat "${_this_dir}"/target/tlscurl-*.jar) > "${_this_dir}/target/tlscurl"
+chmod +x "${_this_dir}/target/tlscurl"
+tar -zcvf "${_this_dir}/target/tlscurl_unix.tar.gz" "${_this_dir}/target/tlscurl"
